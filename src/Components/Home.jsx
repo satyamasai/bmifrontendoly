85fd1ae149b2044d66034a32e458f3b9aaadf057
Satyam gupta <stymngrya@gmail.com>
2022-12-09 22:21:41 +0530

    color mode added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 // import {Navbar}  from './Navbar'
-import { Button } from '@chakra-ui/react'
+import { Button, useColorMode } from '@chakra-ui/react'
 import Logout from './Logout'
 // import Login from './Login'
 import GetProfile from './GetProfile'
 
 const Home = () => {
-
+  const { colorMode, toggleColorMode } = useColorMode()
   const auth= localStorage.getItem("BMI_token") || 0
   // console.log("auth"+auth)
   return (<>
@@ -22,6 +22,10 @@ const Home = () => {
     <Button colorScheme='blue'>My Acoount</Button>
     </a>
     {auth!==0?<Logout/>:<a href='/login'> <Button colorScheme={'blue'}> Login </Button> </a> }
+
+    <Button onClick={toggleColorMode}>
+    Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+  </Button>
     
     </div>
 
